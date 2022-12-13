2dd346cc802660e4e8f9a25e8d6dbe40543eae79
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-13 13:55:45 +0100

    Snack-2

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -5,9 +5,44 @@
 
 
 
+$name = $_GET['name'];
+$email = $_GET['email'];
+$age = $_GET['age'];
 
 
+
+if(strlen($name) > 3 && strpos($email, '@' , '.') !== false && is_numeric($age) == true ){
+    echo 'Accesso Riuscito';
+}else{
+    echo 'Accesso Negato';
+}
 
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-2</title>
+</head>
+<body>
+   
+
+<form action="" method="GET">
+    <input type="text" name="name">
+    <input type="text" name="email">
+    <input type="number" name="age">
+    <button>Send</button>
+</form>
+
+
+
+
+</body>
+</html>
