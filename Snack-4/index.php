d9b1b975d658d9df66890c4058d7d6d2cc6eb04f
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-13 15:53:24 +0100

    Snack-5

--- Snack-4/index.php
+++ Snack-4/index.php
@@ -1,6 +1,31 @@
 <?php 
+
 // Snack 4
 // Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+$randomNumbers=[];
+
+
+
+
+
+while(count($randomNumbers) < 15 ){
+
+    $number = rand(1,100);
+    
+    if(!in_array($number ,$randomNumbers )){
+        $randomNumbers[] = $number;
+    }
+   
+}
+
+var_dump($randomNumbers);
+
+
+
+
+
+
 
 
 
